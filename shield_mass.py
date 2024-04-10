bd0022211dd3e337f14ca7228d9f4e10cae46179
MaryPatience <111059107+MaryPatience@users.noreply.github.com>
2024-04-10 13:59:54 +0100

    Update shield_mass.py
    Edited the shielding coverage

--- shield_mass.py
+++ shield_mass.py
@@ -14,7 +14,7 @@ def detector_shield(length, width, height,length_base, width_base, height_base):
     total_volume = (length_base*width_base*height_base) + (4*volume)
     return total_volume
 
-final = CM_shield(141.42, 0.4, 0.03) + CM_to_detector_shield(0.4,100,141.42,500) + detector_shield(100, 0.4, 0.2,100, 100, 0.4)
-mass_of_shield = final*(11.4+7.28+8.92)
+final = CM_shield(141.42, 0.2, 0.03) + CM_to_detector_shield(0.2,100,141.42,500) + detector_shield(100, 0.7, 0.2,100, 100, 0.7)
+mass_of_shield = (detector_shield(100, 0.7, 0.2,100, 100, 0.7)*(11.4+7.28+8.92)) + (CM_shield(141.42, 0.2, 0.03) + CM_to_detector_shield(0.2,100,141.42,500)*(7.28+8.92))
 print(f"Final volume = {final} cm^3")
 print(f"Shielding mass = {mass_of_shield} g")
